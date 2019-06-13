40a8b0cdac3a48bc6eb4bcc2a3f8b67995e6cc6d
aireat <aireat@gmail.com>
2019-06-13 15:16:09 +0900

    [추가] 자동 생성된 include 파일 추가 함.

---
+++ kernel-5.1.8/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Jun 11 18:48:08 KST 2019"
+#define LINUX_COMPILE_BY "aireat"
+#define LINUX_COMPILE_HOST "linux"
+#define LINUX_COMPILER "gcc version 7.4.0 (Ubuntu/Linaro 7.4.0-1ubuntu1~18.04)"
